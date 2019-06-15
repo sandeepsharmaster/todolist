d9b1a975233d9802e4ca956ce478c6c8216e811d
Abhishek <kumarabhishek0388@gmail.com>
2019-06-15 15:15:37 +0530

    code restructuring

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  items = [];
+  selectedOptions :string[];
+  handleClickAdd(value: string) {
+    if(value) {
+      this.items.push(value);
+    }
+  }
+
+  handleDelete() {
+    console.log(this.selectedOptions)
+    this.selectedOptions.forEach(element => {
+     console.log(element)
+    });
+  }
+}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
