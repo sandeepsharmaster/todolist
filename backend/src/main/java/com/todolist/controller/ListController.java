3a48688b96c2396324de4d66a02318448b298de4
KUMAR Abhishek <kumarabhishek0388@gmail.com>
2019-06-18 18:21:30 +0530

    adding create todoList

--- backend/src/main/java/com/todolist/controller/ListController.java
+++ backend/src/main/java/com/todolist/controller/ListController.java
@@ -1,5 +1,31 @@
 package com.todolist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.todolist.entity.ToDoList;
+import com.todolist.service.ListService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@Api(value = "ToDoList", description = "Operations related to todolist service")
 public class ListController {
+	
+	@Autowired
+	private ListService listService;
+
+	@ApiOperation(value = "Register new User")
+	@PostMapping(value = "/createList")
+	public ResponseEntity<String> registerUser(@RequestBody ToDoList toDoList) {
+		// Save details
+		listService.registerUser(toDoList);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
--- backend/src/main/java/com/todolist/service/ListService.java
+++ backend/src/main/java/com/todolist/service/ListService.java
@@ -1,5 +1,8 @@
 package com.todolist.service;
 
+import com.todolist.entity.ToDoList;
+
 public interface ListService {
 
+	public void createToDoList(ToDoList toDoList);
 }
