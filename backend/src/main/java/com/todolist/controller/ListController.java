99418869c789d6d75bcd93337b41954a40e3677e
KUMAR Abhishek <kumarabhishek0388@gmail.com>
2019-06-26 17:22:08 +0530

    changing mapping to get

--- backend/src/main/java/com/todolist/controller/ListController.java
+++ backend/src/main/java/com/todolist/controller/ListController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -36,7 +37,7 @@ public class ListController {
 	}
 	
 	@ApiOperation(value = "Get list")
-	@PostMapping(value = "/getList")
+	@GetMapping(value = "/getList")
 	public ResponseEntity<List<UserList>> getUserList(@RequestParam Long listId) {
 		List<UserList> userlist= listService.getList(listId);
 		return new ResponseEntity<List<UserList>>(userlist, HttpStatus.OK);
