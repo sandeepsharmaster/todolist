95d7d5f6dca1d4d5d95c2420a4de7fd92cbace59
KUMAR Abhishek <kumarabhishek0388@gmail.com>
2019-06-26 17:34:52 +0530

    clearing textbox after adding element

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,11 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   items = [];
   selectedOptions :string[];
   handleClickAdd(value: string) {
     if(value) {
       this.items.push(value);
+      
     }
   }
 
